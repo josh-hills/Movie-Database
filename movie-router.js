ce110d7e92a5df842bf73696b672bfd5d7ea523d
josh-hills <57681652+josh-hills@users.noreply.github.com>
2021-04-10 10:12:39 -0400

    movie query working

--- movie-router.js
+++ movie-router.js
@@ -10,7 +10,16 @@ let connection = mongoose.connection;
 //Create router
 //if you wanna test this, go grab any valid id and paste it for the id param in find
 movieRouter.get("/", (req, res, next)=> {
-    find('movies', {_id : "6YVMWXmY4t4c0UC2cjuwo"}, function (err, docs) {
+    let id;
+	try{
+		id = req.query.id;
+        console.log(id);
+	}catch{
+		res.status(404).send("Unknown ID");
+		return;
+	}
+
+    find('movies', {_id : id}, function (err, docs) {
         let myMovie = docs[0];
         doRender(req, res, next, myMovie);
     });   

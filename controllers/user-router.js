6065744bca058cf5679aabf851ce374c25be2b7b
josh-hills <57681652+josh-hills@users.noreply.github.com>
2021-04-13 07:26:53 -0400

    follow user

--- controllers/user-router.js
+++ controllers/user-router.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const userRotuer = express.Router();
+const userRouter = express.Router();
 mongoose.connect('mongodb://localhost/moviedb', {useNewUrlParser: true});
 let db = mongoose.connection;
 
 //loops through the users watchlist and adds it to the userWatchlist array, this and userProfile are passed to user.pug
-userRotuer.get("/", async (req, res, next)=> {
+userRouter.get("/", async (req, res, next)=> {
     try{
-        let userProfile = await find("users","username",req.query.username);
+        let userProfile = await find("users","username",req.query.username,res);
         let userWatchlist = [];
         for(let i = 0; i < userProfile.watchlist.length; i++){
             let id = userProfile.watchlist[i].id;
@@ -20,18 +20,51 @@ userRotuer.get("/", async (req, res, next)=> {
     }
 });
 
+
+userRouter.post("/", async (req, res, next)=> {
+    myProfile = await find("users","username",req.session.username, res);
+    let user = await find("users","username",req.query.username, res);
+    if(req.body.unfollow){   
+        for(var i = 0; i < myProfile.followedUsers.length; i++) {
+            if (myProfile.followedUsers[i].username == req.query.username) {
+                myProfile.followedUsers.splice(i, 1);
+                await db.collection("users").updateOne(
+                    {username:myProfile.username},
+                    {
+                        $set:{"followedUsers":myProfile.followedUsers}
+                    }
+                );
+            }
+        }
+        doRender(req, res, next, user, user.watchlist);
+    }else if(req.body.follow){
+        let query = {};
+        query["username"] = req.query.username;
+        myProfile.followedUsers.push(query);
+        await db.collection("users").updateOne(
+            {username:myProfile.username},
+            {
+                $set:{"followedUsers":myProfile.followedUsers}
+            }
+        );
+        doRender(req, res, next, user, user.watchlist);
+    }
+});
+
 //function that queries the id for the passed query, using promises ensures that we can wait for the end of the function
 //i is var to look at (ex. _id), q is query
-function find (coll,i,q) {
+function find (coll,i,q, res) {
     return new Promise((resolve, reject) => {
         let query = {};
         query[i] = q;
         db.collection(coll).findOne(query,function(err, result){
             if(err){
-                reject("Error reading database.");
+                res.status(500).send("Error reading database.");
+                return;
             }
             if(!result){
-                reject("ID not found");
+                res.status(404).send("Unknown ID");
+                return;
             }
             resolve(result);
         })
@@ -39,8 +72,18 @@ function find (coll,i,q) {
 }
 
 //renders user page
-function doRender(req, res, next, userProfile, userWatchlist){
-    res.render("pages/user", {userProfile, userWatchlist}); 
+async function doRender(req, res, next, userProfile, userWatchlist){
+    let following;
+    if(req.session.loggedin){
+        following = false;
+        myProfile = await find("users","username",req.session.username, res);
+        for(var i = 0; i < myProfile.followedUsers.length; i++) {
+            if (myProfile.followedUsers[i].username == userProfile.username) {
+                following = true;
+            }
+        }
+    }
+    res.render("pages/user", {userProfile, userWatchlist, following}); 
 }
 
-module.exports = userRotuer;
+module.exports = userRouter;
